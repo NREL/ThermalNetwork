14baacd1c22c0aa3d312ad3f39749355b21715c0
Matt Mitchell <matt.mitchell@nrel.gov>
2023-07-19 10:26:23 -0600

    update reqt

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'Programming Language :: Python :: 3.11'
     ],
     install_requires=[
-        'click>=8.1.3',
+        'click>=8.1.6',
         'ghedesigner>=1.1'
     ],
     entry_points={
