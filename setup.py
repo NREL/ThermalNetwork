b5691f12a2f1a0f11ff899324ca3a34dd9ba2929
Matt Mitchell <matt.mitchell@nrel.gov>
2023-07-19 10:51:52 -0600

    prep for release

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+from pathlib import Path
+
 from setuptools import setup
 
 from thermalnetwork import VERSION
 
 short_description = """A thermal network solver for GHE sizing."""
 
+readme_file = Path(__file__).parent.resolve() / 'README.md'
+readme_contents = readme_file.read_text(encoding='utf8')
+
 setup(
     name='ThermalNetwork',
     url='https://github.com/mitchute/ThermalNetwork',
     description=short_description,
+    long_description=readme_contents,
+    long_description_content_type='text/markdown',
     license='BSD-3',
     version=VERSION,
     packages=['thermalnetwork'],
@@ -17,7 +24,7 @@ setup(
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11'
     ],
     install_requires=[
