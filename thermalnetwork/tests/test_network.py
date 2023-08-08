8fa912febbd84436e25dbfe57d06c9d10d266f25
brianlball <brian.ball@nrel.gov>
2023-08-08 12:38:06 -0400

    add test for UPSTREAM when GHE is passed no loads

--- thermalnetwork/tests/test_network.py
+++ thermalnetwork/tests/test_network.py
@@ -38,6 +38,22 @@ class TestNetwork(BaseCase):
 
         run_sizer_from_cli_worker(system_parameter_path, scenario_directory_path, geojson_file_path, output_path)
 
+    def test_network_two_ghe_no_load(self):
+        geojson_file_path = self.demos_path / 'sdk_output_skeleton' / 'example_project_combine_GHE_2.json'
+        geojson_file_path = geojson_file_path.resolve()
+
+        scenario_directory_path = self.demos_path / 'sdk_output_skeleton' / 'run' / 'baseline_scenario'
+        scenario_directory_path.resolve()
+
+        system_parameter_path = scenario_directory_path / 'ghe_dir' / 'system_parameter_upstream.json'
+        system_parameter_path.resolve()
+
+        output_path = self.test_outputs_path / 'two_ghe_no_load'
+        if not output_path.exists():
+            os.mkdir(output_path)
+
+        run_sizer_from_cli_worker(system_parameter_path, scenario_directory_path, geojson_file_path, output_path)    
+
     def test_network_two_ghe_from_UO(self):
         geojson_file_path = self.demos_path / 'sdk_output_skeleton_2' / 'example_project_with_ghe.json'
         geojson_file_path = geojson_file_path.resolve()
