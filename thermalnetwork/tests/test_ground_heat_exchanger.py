5360f1cc458d064ee13ae8c4d71630cbaf179ab7
Nathan Moore <nathan.moore@nrel.gov>
2023-11-02 11:54:21 -0600

    enable a first test of GHE

--- thermalnetwork/tests/test_ground_heat_exchanger.py
+++ thermalnetwork/tests/test_ground_heat_exchanger.py
@@ -1,18 +1,16 @@
-# from unittest import TestCase
-#
-# from thermalnetwork.ground_heat_exchanger import GHE
-#
-#
-# class TestGHE(TestCase):
-#     def test_calc_src_side_load(self):
-#         data = {
-#             "name": "GHE",
-#             "type": "GROUNDHEATEXCHANGER",
-#             "properties": {
-#                 "length": 20,
-#                 "width": 50
-#             }
-#         }
-#         ghe = GHE(data)
-#         self.assertEquals(ghe.name, 'GHE')
-#         self.assertEquals(ghe.area, 1000)
+from thermalnetwork.ground_heat_exchanger import GHE
+from thermalnetwork.tests.test_base import BaseCase
+
+
+class TestGHE(BaseCase):
+    def test_calc_src_side_load(self):
+        data = {
+            "name": "GHE",
+            "id": "asdfjkl;",
+            "type": "GROUNDHEATEXCHANGER",
+            "properties": {"geometric_constraints": {"length": 20, "width": 50}},
+        }
+        ghe = GHE(data)
+        assert ghe.name == "GHE"
+        assert ghe.area == 1000
+        assert ghe.id == "asdfjkl;"
