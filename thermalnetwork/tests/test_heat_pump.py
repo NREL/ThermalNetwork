27ea90ba29971bbb79184e4fdbffadd3bcfa7f2a
Nathan Moore <nathan.moore@nrel.gov>
2025-05-01 17:06:50 -0600

    test heating and cooling heat pumps separately

--- thermalnetwork/tests/test_heat_pump.py
+++ thermalnetwork/tests/test_heat_pump.py
@@ -1,6 +1,6 @@
 import pytest
 
-from thermalnetwork.heat_pump import HeatPump
+from thermalnetwork.heat_pump import HP
 from thermalnetwork.tests.test_base import BaseCase
 
 
@@ -10,9 +10,10 @@ class TestHeatPump(BaseCase):
         data = {"name": "WSHP", "type": "HEATPUMP", "properties": {"cop_c": 3.5, "cop_h": 2.5}}
 
         # -- Run
-        hp = HeatPump(data)
+        heating_hp = HP(data["name"], data["properties"]["cop_h"], "heating")
+        cooling_hp = HP(data["name"], data["properties"]["cop_c"], "cooling")
 
         # -- Check
-        assert hp.name == "WSHP"
-        assert hp.calc_src_side_load(1) == pytest.approx(0.60, 0.01)
-        assert hp.calc_src_side_load(-1) == pytest.approx(-1.28, 0.01)
+        assert heating_hp.name == "WSHP"
+        assert heating_hp.calc_src_side_load(1) == pytest.approx(0.60, 0.01)
+        assert cooling_hp.calc_src_side_load(-1) == pytest.approx(-1.28, 0.01)
