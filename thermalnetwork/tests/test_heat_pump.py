50b3e9c6d3f01943c536ee487f641f98bf6f6956
Nathan Moore <nathan.moore@nrel.gov>
2025-05-01 17:09:12 -0600

    add heat pump test for dhw

--- thermalnetwork/tests/test_heat_pump.py
+++ thermalnetwork/tests/test_heat_pump.py
@@ -7,13 +7,15 @@ from thermalnetwork.tests.test_base import BaseCase
 class TestHeatPump(BaseCase):
     def test_calc_src_side_load(self):
         # -- Set up
-        data = {"name": "WSHP", "type": "HEATPUMP", "properties": {"cop_c": 3.5, "cop_h": 2.5}}
+        data = {"name": "WSHP", "type": "HEATPUMP", "properties": {"cop_c": 3.5, "cop_h": 2.5, "cop_dhw": 2.5}}
 
         # -- Run
         heating_hp = HP(data["name"], data["properties"]["cop_h"], "heating")
         cooling_hp = HP(data["name"], data["properties"]["cop_c"], "cooling")
+        dhw_hp = HP(data["name"], data["properties"]["cop_dhw"], "dhw")
 
         # -- Check
         assert heating_hp.name == "WSHP"
         assert heating_hp.calc_src_side_load(1) == pytest.approx(0.60, 0.01)
         assert cooling_hp.calc_src_side_load(-1) == pytest.approx(-1.28, 0.01)
+        assert dhw_hp.calc_src_side_load(1) == pytest.approx(0.60, 0.01)
