c10604afbf9fd496f9881756934dfb19a838f02e
Matt Mitchell <matt.mitchell@nrel.gov>
2025-05-28 17:39:13 -0600

    wip - nearly there...

--- thermalnetwork/tests/test_base.py
+++ thermalnetwork/tests/test_base.py
@@ -1,10 +1,7 @@
-import json
 from pathlib import Path
 from unittest import TestCase
 
 from click.testing import CliRunner
-
-from thermalnetwork.utilities import write_json
 
 
 class BaseCase(TestCase):
@@ -68,7 +65,7 @@ class BaseCase(TestCase):
         ).resolve()
 
         # 13 buildings area proportional
-        self.system_parameter_path_13_buildings_proportional_ghe = (
+        self.sys_param_path_13_buildings_proportional_ghe = (
             self.scenario_dir_13_buildings / "ghe_dir" / "sys_params_proportional.json"
         ).resolve()
 
@@ -77,57 +74,19 @@ class BaseCase(TestCase):
             self.scenario_directory_path_1_ghe / "ghe_dir" / "sys_params_pre_designed.json"
         ).resolve()
 
+        # 2 ghe pre-designed
+        self.sys_param_path_2_ghe_pre_designed = (
+            self.scenario_directory_path_1_ghe / "ghe_dir" / "sys_params_pre_designed.json"
+        ).resolve()
+
+        self.geojson_path_2_ghe_pre_designed = (
+            self.demos_path / "sdk_output_skeleton_2_ghe_pre_designed" / "network.geojson"
+        ).resolve()
+
+        self.scenario_dir_2_ghe_pre_designed = (
+            self.demos_path / "sdk_output_skeleton_2_ghe_pre_designed" / "run" / "baseline_scenario"
+        ).resolve()
+
         # -- Output paths
         self.test_outputs_path = here.resolve() / "test_outputs"
         self.test_outputs_path.mkdir(exist_ok=True)
-
-        # Save the original sys-param data, so they can be restored after the tests run.
-        sys_param_1_ghe = json.loads(self.sys_param_path_1_ghe.read_text())
-
-        district_params = sys_param_1_ghe["district_system"]["fifth_generation"]
-        self.original_hydraulic_diameter = district_params["horizontal_piping_parameters"]["hydraulic_diameter"]
-        self.original_pump_design_head = district_params["central_pump_parameters"]["pump_design_head"]
-        self.original_pump_flow_rate = district_params["central_pump_parameters"]["pump_flow_rate"]
-
-        one_ghe_specific_params = sys_param_1_ghe["district_system"]["fifth_generation"]["ghe_parameters"][
-            "ghe_specific_params"
-        ]
-        for ghe in one_ghe_specific_params:
-            # These values are the same across all our demo files.
-            self.original_borehole_length = ghe["borehole"]["length_of_boreholes"]
-            self.original_num_boreholes = ghe["borehole"]["number_of_boreholes"]
-
-    def check_outputs(self, output_path: Path, expected_data: dict):
-        for ghe_id in output_path.iterdir():
-            if ghe_id.is_dir():
-                sim_summary = json.loads((ghe_id / "SimulationSummary.json").read_text())
-                actual_depth = sim_summary["ghe_system"]["active_borehole_length"]["value"]
-                actual_nbh = sim_summary["ghe_system"]["number_of_boreholes"]
-                expected_depth = expected_data[ghe_id.name]["depth"]
-                expected_nbh = expected_data[ghe_id.name]["num_bh"]
-                self.assertEqual(actual_nbh, expected_nbh)
-                self.assertAlmostEqual(actual_depth, expected_depth, delta=0.1)
-
-    def reset_sys_param(self, sys_param_path: Path):
-        sys_param = json.loads(sys_param_path.read_text())
-        sys_param["district_system"]["fifth_generation"]["horizontal_piping_parameters"]["hydraulic_diameter"] = (
-            self.original_hydraulic_diameter
-        )
-        sys_param["district_system"]["fifth_generation"]["central_pump_parameters"]["pump_design_head"] = (
-            self.original_pump_design_head
-        )
-        sys_param["district_system"]["fifth_generation"]["central_pump_parameters"]["pump_flow_rate"] = (
-            self.original_pump_flow_rate
-        )
-
-        ghe_specific_params = sys_param["district_system"]["fifth_generation"]["ghe_parameters"]["ghe_specific_params"]
-
-        for idx, _ in enumerate(ghe_specific_params):
-            sys_param["district_system"]["fifth_generation"]["ghe_parameters"]["ghe_specific_params"][idx]["borehole"][
-                "length_of_boreholes"
-            ] = self.original_borehole_length
-            sys_param["district_system"]["fifth_generation"]["ghe_parameters"]["ghe_specific_params"][idx]["borehole"][
-                "number_of_boreholes"
-            ] = self.original_num_boreholes
-
-        write_json(sys_param_path, sys_param)
--- thermalnetwork/tests/test_network.py
+++ thermalnetwork/tests/test_network.py
@@ -1,8 +1,71 @@
-from thermalnetwork.network import run_sizer_from_cli_worker
+from thermalnetwork.network import run_sizer_from_cli, run_sizer_from_cli_worker
 from thermalnetwork.tests.test_base import BaseCase
+from thermalnetwork.utilities import load_json
 
 
 class TestNetwork(BaseCase):
+    def check_horiz_pipe_params(self, sys_param: dict, expected_hydraulic_dia: float):
+        self.assertAlmostEqual(
+            sys_param["district_system"]["fifth_generation"]["horizontal_piping_parameters"]["hydraulic_diameter"],
+            expected_hydraulic_dia,
+            delta=0.0001,
+        )
+
+    def check_pump_params(self, sys_param: dict, expected_pump_head: float, expected_flow_rate: float):
+        self.assertAlmostEqual(
+            sys_param["district_system"]["fifth_generation"]["central_pump_parameters"]["pump_design_head"],
+            expected_pump_head,
+            delta=1,
+        )
+
+        self.assertAlmostEqual(
+            sys_param["district_system"]["fifth_generation"]["central_pump_parameters"]["pump_flow_rate"],
+            expected_flow_rate,
+            delta=0.01,
+        )
+
+    def check_ghe_data(self, sys_param: dict, expected_ghe_data: dict):
+        ghe_data = sys_param["district_system"]["fifth_generation"]["ghe_parameters"]["ghe_specific_params"]
+        for ghe in ghe_data:
+            for k_expected, v_expected in expected_ghe_data.items():
+                if ghe["ghe_id"] == k_expected:
+                    self.assertAlmostEqual(ghe["borehole"]["length_of_boreholes"], v_expected["length"], delta=0.1)
+                    self.assertEqual(ghe["borehole"]["number_of_boreholes"], v_expected["num_bh"])
+
+    def test_cli(self):
+        output_path = self.test_outputs_path.resolve() / "cli_test"
+
+        # -- Act
+        # run subprocess as if we're an end-user
+        res = self.runner.invoke(
+            run_sizer_from_cli,
+            [
+                "-y",
+                self.sys_param_path_1_ghe,
+                "-s",
+                self.scenario_directory_path_1_ghe,
+                "-f",
+                self.geojson_path_1_ghe,
+                "-o",
+                output_path,
+            ],
+        )
+
+        # -- Assert
+        assert res.exit_code == 0
+
+        updated_sys_param = load_json(self.sys_param_path_1_ghe)
+
+        expected_hydraulic_dia = 0.13767
+        expected_pump_head = 128704
+        expected_flow_rate = 0.025
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {"8c369df2-18e9-439a-8c25-875851c5aaf0": {"num_bh": 50, "length": 89.5}}
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
+
     def test_network_one_ghe(self):
         # -- Set up
         output_path = self.test_outputs_path / "one_ghe"
@@ -16,10 +79,17 @@ class TestNetwork(BaseCase):
             output_path,
         )
 
-        expected_outputs = {"8c369df2-18e9-439a-8c25-875851c5aaf0": {"num_bh": 50, "depth": 89.5}}
+        updated_sys_param = load_json(self.sys_param_path_1_ghe)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.sys_param_path_1_ghe)
+        expected_hydraulic_dia = 0.13767
+        expected_pump_head = 128704
+        expected_flow_rate = 0.025
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {"8c369df2-18e9-439a-8c25-875851c5aaf0": {"num_bh": 50, "length": 89.5}}
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
 
     def test_network_one_ghe_detailed_geometry(self):
         # -- Set up
@@ -34,10 +104,17 @@ class TestNetwork(BaseCase):
             output_path,
         )
 
-        expected_outputs = {"8c369df2-18e9-439a-8c25-875851c5aaf0": {"num_bh": 36, "depth": 128.1}}
+        updated_sys_param = load_json(self.sys_param_path_1_ghe_detailed_geometry)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.sys_param_path_1_ghe_detailed_geometry)
+        expected_hydraulic_dia = 0.11560
+        expected_pump_head = 164720
+        expected_flow_rate = 0.018
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {"8c369df2-18e9-439a-8c25-875851c5aaf0": {"num_bh": 36, "length": 128.1}}
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
 
     def test_network_two_ghe_sequential(self):
         # -- Set up
@@ -52,13 +129,20 @@ class TestNetwork(BaseCase):
             output_path,
         )
 
-        expected_outputs = {
-            "dd69549c-ecfc-4245-96dc-5b6127f34f46": {"num_bh": 16, "depth": 127.4},
-            "47fd01d3-3d72-46c0-85f2-a12854783764": {"num_bh": 13, "depth": 127.9},
-        }
+        updated_sys_param = load_json(self.sys_param_path_2_ghe_sequential)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.sys_param_path_2_ghe_sequential)
+        expected_hydraulic_dia = 0.09351
+        expected_pump_head = 160804
+        expected_flow_rate = 0.008
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {
+            "dd69549c-ecfc-4245-96dc-5b6127f34f46": {"num_bh": 16, "length": 127.4},
+            "47fd01d3-3d72-46c0-85f2-a12854783764": {"num_bh": 13, "length": 127.9},
+        }
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
 
     def test_network_two_ghe_staggered(self):
         # -- Set up
@@ -73,13 +157,20 @@ class TestNetwork(BaseCase):
             output_path,
         )
 
-        expected_outputs = {
-            "dd69549c-ecfc-4245-96dc-5b6127f34f46": {"num_bh": 16, "depth": 127.3},
-            "47fd01d3-3d72-46c0-85f2-a12854783764": {"num_bh": 13, "depth": 127.7},
-        }
+        updated_sys_param = load_json(self.system_parameter_path_2_ghe_staggered)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.system_parameter_path_2_ghe_staggered)
+        expected_hydraulic_dia = 0.09351
+        expected_pump_head = 188970
+        expected_flow_rate = 0.01
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {
+            "dd69549c-ecfc-4245-96dc-5b6127f34f46": {"num_bh": 16, "length": 127.3},
+            "47fd01d3-3d72-46c0-85f2-a12854783764": {"num_bh": 13, "length": 127.7},
+        }
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
 
     def test_network_three_ghe_upstream(self):
         # -- Set up
@@ -94,14 +185,21 @@ class TestNetwork(BaseCase):
             output_path,
         )
 
-        expected_outputs = {
-            "344421ab-b416-403e-bd22-7b8af7b581a2": {"num_bh": 2380, "depth": 135.0},
-            "8410c9a9-d52e-4c68-b7bc-4affae974481": {"num_bh": 72, "depth": 93.0},
-            "3eb26af6-a1f7-4daa-8372-ec016ca185a4": {"num_bh": 2451, "depth": 135.0},
-        }
+        updated_sys_param = load_json(self.sys_param_13_buildings_upstream_ghe)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.sys_param_13_buildings_upstream_ghe)
+        expected_hydraulic_dia = 0.54032
+        expected_pump_head = 782301
+        expected_flow_rate = 1.2255
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {
+            "344421ab-b416-403e-bd22-7b8af7b581a2": {"num_bh": 2380, "length": 135.0},
+            "8410c9a9-d52e-4c68-b7bc-4affae974481": {"num_bh": 72, "length": 93.0},
+            "3eb26af6-a1f7-4daa-8372-ec016ca185a4": {"num_bh": 2451, "length": 135.0},
+        }
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
 
     def test_network_three_ghe_area_proportional(self):
         # -- Set up
@@ -110,20 +208,27 @@ class TestNetwork(BaseCase):
 
         # -- Run
         run_sizer_from_cli_worker(
-            self.system_parameter_path_13_buildings_proportional_ghe,
+            self.sys_param_path_13_buildings_proportional_ghe,
             self.scenario_dir_13_buildings,
             self.geojson_path_13_buildings,
             output_path,
         )
 
-        expected_outputs = {
-            "344421ab-b416-403e-bd22-7b8af7b581a2": {"num_bh": 2380, "depth": 135.0},
-            "8410c9a9-d52e-4c68-b7bc-4affae974481": {"num_bh": 2475, "depth": 135.0},
-            "3eb26af6-a1f7-4daa-8372-ec016ca185a4": {"num_bh": 1036, "depth": 133.5},
-        }
+        updated_sys_param = load_json(self.sys_param_path_13_buildings_proportional_ghe)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.system_parameter_path_13_buildings_proportional_ghe)
+        expected_hydraulic_dia = 0.54032
+        expected_pump_head = 796483
+        expected_flow_rate = 1.2375
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {
+            "344421ab-b416-403e-bd22-7b8af7b581a2": {"num_bh": 2380, "length": 135.0},
+            "8410c9a9-d52e-4c68-b7bc-4affae974481": {"num_bh": 2475, "length": 135.0},
+            "3eb26af6-a1f7-4daa-8372-ec016ca185a4": {"num_bh": 1036, "length": 133.5},
+        }
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
 
     def test_one_ghe_pre_designed(self):
         # -- Set up
@@ -138,10 +243,17 @@ class TestNetwork(BaseCase):
             output_path,
         )
 
-        expected_outputs = {"8c369df2-18e9-439a-8c25-875851c5aaf0": {"num_bh": 20, "depth": 89.5}}
+        updated_sys_param = load_json(self.sys_param_path_1_ghe_pre_designed)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.sys_param_path_2_ghe_sequential)
+        expected_hydraulic_dia = 0.09351
+        expected_pump_head = 158326
+        expected_flow_rate = 0.01
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {"8c369df2-18e9-439a-8c25-875851c5aaf0": {"num_bh": 20, "length": 152}}
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
 
     def test_network_two_ghe_pre_designed(self):
         # -- Set up
@@ -150,16 +262,23 @@ class TestNetwork(BaseCase):
 
         # -- Run
         run_sizer_from_cli_worker(
-            self.sys_param_path_2_ghe_sequential,
-            self.scenario_dir_2_ghe_sequential,
-            self.geojson_path_2_ghe_sequential,
+            self.sys_param_path_2_ghe_pre_designed,
+            self.scenario_dir_2_ghe_pre_designed,
+            self.geojson_path_2_ghe_pre_designed,
             output_path,
         )
 
-        expected_outputs = {
-            "dd69549c-ecfc-4245-96dc-5b6127f34f46": {"num_bh": 16, "depth": 127.4},
-            "47fd01d3-3d72-46c0-85f2-a12854783764": {"num_bh": 13, "depth": 127.9},
-        }
+        updated_sys_param = load_json(self.sys_param_path_1_ghe_pre_designed)
 
-        self.check_outputs(output_path, expected_outputs)
-        self.reset_sys_param(self.sys_param_path_2_ghe_sequential)
+        expected_hydraulic_dia = 0.09351
+        expected_pump_head = 158326
+        expected_flow_rate = 0.01
+
+        self.check_horiz_pipe_params(updated_sys_param, expected_hydraulic_dia)
+        self.check_pump_params(updated_sys_param, expected_pump_head, expected_flow_rate)
+
+        expected_ghe_data = {
+            "dd69549c-ecfc-4245-96dc-5b6127f34f46": {"num_bh": 16, "length": 127.4},
+            "47fd01d3-3d72-46c0-85f2-a12854783764": {"num_bh": 13, "length": 127.9},
+        }
+        self.check_ghe_data(updated_sys_param, expected_ghe_data)
